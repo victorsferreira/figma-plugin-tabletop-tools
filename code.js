65c37c60d86a68362096f95d4ede1d74d7675a6b
Victor Ferreira <victorferreirait@gmail.com>
2021-07-13 01:25:04 -0300

    Initial commit

---
+++ code.js
@@ -0,0 +1,121 @@
+figma.showUI(__html__);
+
+let pluginElements = [];
+
+figma.ui.onmessage = async msg => {
+    const page = figma.currentPage;
+    const surface = page.selection[0];
+    const { selection } = page;
+
+    pluginElements.forEach(el => {
+        el.remove();
+    });
+
+    pluginElements = [];
+
+    if (
+        !page.selection || page.selection.length === 0
+    ) {
+        alert('Select a surface!')
+        return false;
+    }
+
+    await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+    if (msg.type === 'roll-dice') {
+        rollDices(msg.sides, msg.amount, { surface, page });
+    } else if (msg.type === 'set-hourglass') {
+        setHourglass(msg.length, { surface, page });
+    } else if (msg.type === 'reveal-one') {
+        revealOne({ surface, page, selection });
+    }
+};
+
+function revealOne({ surface, page, selection }) {
+    const selected = selection[0];
+
+    const elements = (
+        selection.length === 1 && selected.type === 'GROUP'
+    ) ? selected.children : selection;
+    
+    const chosenIndex = randomize(0, elements.length);
+    const chosen = elements[chosenIndex];
+    
+    const { width, height, x, y } = chosen;
+    const outline = figma.createRectangle();
+
+    outline.fills = [{ type: 'SOLID', opacity: 0.01, color: { r: 0, g: 0, b: 0 } }];
+    outline.strokeAlign = 'OUTSIDE';
+    outline.strokeWeight = Math.ceil(width / 10);
+    outline.strokes = [
+        { type: 'SOLID', color: { r: 1, g: 1, b: 0 } }
+    ];
+
+    outline.resize(width + 20, height + 20)
+    outline.x = x - 10;
+    outline.y = y - 10;
+
+    pluginElements.push(outline);
+    page.appendChild(outline);
+}
+
+async function setHourglass(length, { surface, page }) {
+    const shape = figma.createRectangle();
+    shape.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
+
+    const timeText = figma.createText();
+    timeText.fontSize = 50;
+    timeText.characters = length.toString();
+
+    const hourglass = figma.group([shape, timeText], page);
+    hourglass.x = surface.x;
+    hourglass.y = surface.y;
+
+    pluginElements.push(hourglass);
+    page.appendChild(hourglass);
+
+    figma.ui.hide();
+
+    const interval = setInterval(() => {
+        let c = parseInt(timeText.characters);
+        c--;
+        timeText.characters = c.toString();
+
+        if (c === 0) {
+            clearInterval(interval);
+            interval = null;
+            figma.ui.postMessage('hourglass-stop');
+            figma.closePlugin();
+        }
+    }, 1000);
+}
+
+async function rollDices(sides, amount, { surface, page }) {
+    const nodes = [];
+
+    for (let i = 0; i < amount; i++) {
+        const shape = figma.createRectangle();
+        shape.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
+
+        const text = figma.createText();
+        text.fontSize = 64;
+        text.characters = randomize(1, sides).toString();
+
+        const dice = figma.group([shape, text], page);
+        dice.x = surface.x + (i * (dice.width+50));
+        dice.y = surface.y;
+
+        page.appendChild(dice);
+
+        pluginElements.push(dice);
+        nodes.push(dice);
+    }
+
+    page.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+
+    figma.closePlugin();
+}
+
+function randomize(min, max) {
+    return Math.floor(Math.random() * max) + min;
+}
